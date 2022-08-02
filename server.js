3245d5c2804337135ee4fb914c28403f5ee70f48
Looper Project Showcases <87170787+LooperShowcase@users.noreply.github.com>
2022-08-02 18:39:42 +0300

    Update server.js

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const app = express();
 const ourWord = "world";
-
+const port = process.env.PORT || 3000
 app.get("/wordle/:guess", function (req, res) {
   let ourWordMap = {
     w: 1,
@@ -36,4 +36,4 @@ app.get("/wordle/:guess", function (req, res) {
   res.send(resArr);
 });
 app.use(express.static("public"));
-app.listen(3000);
+app.listen(port);
